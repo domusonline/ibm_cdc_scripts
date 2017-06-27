af2aba66fafe9ff27a7500a5e822bb6066f007a7
Fernando Nunes <domusonline@gmail.com>
2017-06-27 07:41:21 +0100

    - ALARM_COUNT_LIMIT -> ALERT_COUNT_LIMIT

--- scripts/mon_cdc.sh
+++ scripts/mon_cdc.sh
@@ -2,8 +2,8 @@
 # Copyright (c) 2017 Fernando Nunes
 # License: This script is licensed as Apache ( http://www.apache.org/licenses/LICENSE-2.0.html )
 # $Author: Fernando Nunes - domusonline@gmail.com $
-# $Revision: 1.0.47 $
-# $Date 2017-06-22 13:21:48$
+# $Revision: 1.0.50 $
+# $Date 2017-06-27 07:41:21$
 # Disclaimer: This software is provided AS IS, without any kind of guarantee. Use at your own risk.
 
 #------------------------------------------------------------------------------
@@ -96,7 +96,7 @@ check_instance_running()
 		echo $ALARM_COUNT > ${TMP_DIR}/.${PROGNAME}_alarm_count_${RUN_INSTANCE_NAME}
 		log INFO "CDC instance $RUN_INSTANCE_NAME is not running! Alarm count: $ALARM_COUNT"
 		alert_notification ${RUN_INSTANCE_NAME} DEFAULT ${RUN_INSTANCE_NAME} DEFAULT "Fatal" DEFAULT "CDC instance $RUN_INSTANCE_NAME is not running!"
-		if [ ${ALARM_COUNT} -le ${ALARM_COUNT_LIMIT} ]
+		if [ ${ALARM_COUNT} -le ${ALERT_COUNT_LIMIT} ]
 		then
 			alert_mail "$CURR_DATE CDC instance ${RUN_INSTANCE_NAME} is not running! ALARM COUNT: ${ALARM_COUNT}"
 		fi
@@ -135,7 +135,7 @@ check_instance_running()
 PROGNAME=`basename $0`
 SCRIPT_DIR=`dirname $0`
 TMP_DIR=${SCRIPT_DIR}/tmp
-VERSION=`echo "$Revision: 1.0.47 $" | cut -f2 -d' '`
+VERSION=`echo "$Revision: 1.0.50 $" | cut -f2 -d' '`
 
 # Read the settings from the properties file
 if [ -x $SCRIPT_DIR/conf/cdc.properties ]
